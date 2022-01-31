46de626c0247ca207efbc04cfaf993d1f5b66a69
YerardinPerlaza <yerardinpm@gmail.com>
2022-01-31 18:17:41 -0500

    fix parenthesis task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,5 +7,5 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes a string k and an int OR float v as arguments and returns a tuple.
     The 1 element of the tuple is the string k.
     The 2 element is the square of v and should be annotated as a float."""
-    tuple_kv = [k, v * v]
+    tuple_kv = (k, v * v)
     return tuple_kv
