9dc758578f8b3d1f81676b43853a13f21146310b
YerardinPerlaza <yerardinpm@gmail.com>
2022-02-01 17:29:46 -0500

    correct task 4 to pycode

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,13 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """calls wait_random and spawns it n times asynchronously"""
-    time_list = []
+    queue, array = [], []
+
     for _ in range(n):
-        time_list.append(task_wait_random(max_delay))
-    return [await time for time in asyncio.as_completed(time_list)]
+        queue.append(task_wait_random(max_delay))
+
+    for q in asyncio.as_completed(queue):
+        result = await q
+        array.append(result)
+
+    return array
