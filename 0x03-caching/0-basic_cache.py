897a9ce9c7fa5bdcc01c9b66f451cce810b4aefd
YerardinPerlaza <yerardinpm@gmail.com>
2022-02-05 14:33:13 -0500

    modify conditionals

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -5,19 +5,13 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """caching a system"""
-
-
     def put(self, key, item):
         """Assign to the dictionary self.cache_data the item and the key"""
-        if (key and item):
+        if key is not None and item is not None:
             self.cache_data[key] = item
-
 
     def get(self, key):
         """return value in self.cache_data linked to key"""
-        if key:
-            for k in self.cache_data.keys():
-                if key == k:
-                    return self.cache_data.get(key)
-        return None
-        
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)      
