b23055d6848a26758aa108c61ec3d927e409b0ae
YerardinPerlaza <yerardinpm@gmail.com>
2022-02-05 15:20:10 -0500

    modifi task 4

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -5,8 +5,6 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class MRUCache(BaseCaching):
     """MRU caching system"""
-
-
     def __init__(self):
         """Initialize"""
         super().__init__()
@@ -14,23 +12,22 @@ class MRUCache(BaseCaching):
 
     def put(self, key, item):
         """assign to the self-cache_data the item value for the key"""
-        if (key and item):
-            self.cache_data[key] = item
-            if key in self.keys:
-                self.keys.remove(key)
-                self.keys.append(key)
-            else:
-                self.keys.append(key)
-            if len(self.keys) > self.MAX_ITEMS:
-                i = self.MAX_ITEMS -1
-                out = self.keys.pop(i)
-                del self.cache_data[out]
-                print('DISCAR: {}'.format(out))
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            out = self.keys.pop()
+            del self.cache_data[out]
+            print('DISCARD: {}'.format(out))
+
+        self.keys.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """return the value in self.cache_data to key"""
-        if key and key in self.keys:
-            self.keys.remove(key)
-            self.keys.append(key)
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
