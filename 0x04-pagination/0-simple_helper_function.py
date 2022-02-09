5dcfda98e6bede914c4b408d6f65c417233000c4
YerardinPerlaza <yerardinpm@gmail.com>
2022-02-08 20:16:45 -0500

    solve mandatory task pagination

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function named index_range"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Simple helper to pagination, takes two integer arguments page and page_size"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
