f46879b6ae817297f3046168f069e76ea7989a3a
YerardinPerlaza <yerardinpm@gmail.com>
2022-02-17 20:53:26 -0500

    modify messages

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -7,11 +7,28 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if os.getenv("AUTH_TYPE") == 'basic_auth':
+    auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == 'session_auth':
+    auth = SessionAuth()
+elif os.getenv("AUTH_TYPE") == 'session_exp_auth':
+    auth = SessionExpAuth()
+elif os.getenv("AUTH_TYPE") == 'session_db_auth':
+    auth = SessionDBAuth()
+else:
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -19,6 +36,43 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """ Not Authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Before_request handler
+    """
+
+    if auth is None:
+        return
+
+    _paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+              '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, _paths):
+        return
+
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
+        abort(401)
+
+    request.current_user = auth.current_user(request)
+
+    if request.current_user is None:
+        abort(403)
 
 
 if __name__ == "__main__":
