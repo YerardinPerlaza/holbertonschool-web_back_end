39d29a5eea672283685ed93018ccb29cbb8ddf8a
YerardinPerlaza <yerardinpm@gmail.com>
2022-03-28 17:40:50 -0500

    changefile

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
 export function taskFirst() {
     const task = 'I prefer const when I can.';
-    return task;   
+    return task;
 }
 export function getLast() {
     return ' is okay';
@@ -9,5 +9,5 @@ export function getLast() {
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-    return combination;
+    return combination;   
 }
